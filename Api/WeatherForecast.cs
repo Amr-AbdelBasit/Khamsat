d8a35627e2fe9b2325f39ec3fbd58ea5811e4997
Walaa Mohamed <wm205mohamed@gmail.com>
2021-05-27 00:05:34 +0200

    Test Commit

--- Api/WeatherForecast.cs
+++ Api/WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace Api
     {
         public DateTime Date { get; set; }
 
+        public string test { get; set; }
+
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
